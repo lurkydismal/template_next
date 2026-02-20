fcaf422775395e10e2979ca252a87cfd14331ae0
lurkydismal <lurkydismal@duck.com>
2026-02-20 15:40:02 +0500

    1

--- drizzle/20260121104816_fixed_scarecrow/migration.sql
+++ drizzle/20260121104816_fixed_scarecrow/migration.sql
@@ -1,3 +1,11 @@
+-- Tables
+CREATE TABLE "table" (
+	"id" serial PRIMARY KEY,
+	"content" text NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+
 -- Functions
 CREATE OR REPLACE FUNCTION update_updated_at_column()
 RETURNS TRIGGER AS $$
@@ -6,14 +14,6 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
--- Tables
-CREATE TABLE "table" (
-	"id" serial PRIMARY KEY,
-	"content" text NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
 
 -- Register triggers
 CREATE TRIGGER update_table_updated_at

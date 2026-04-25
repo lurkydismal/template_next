c045eaebfe050da7abfb75f4d65583e68daf0d9b
LurkyDismal <lurkydismal@duck.com>
2026-04-25 08:05:14 +0000

    feat(lib): refactor types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,11 @@
 import z from "zod";
-import { table } from "@/db/schema";
+import { users, follows, categories, posts } from "@/db/schema";
 
 export const TABLES = {
-    table: table,
+    users: users,
+    follows: follows,
+    categories: categories,
+    posts: posts,
 } as const;
 
 export type DbTarget = keyof typeof TABLES;
@@ -21,6 +24,7 @@ export function parseRawTarget(rawTarget: DbTarget) {
     return table;
 }
 
-export type ActionResult =
-    | { ok: true; data?: any }
-    | { ok: false; error: string };
+type SuccessResult<T> = { ok: true; data?: T };
+type FailureResult = { ok: false; error: string };
+
+export type ActionResult<T = void> = SuccessResult<T> | FailureResult;

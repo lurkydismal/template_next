c0d88c43b72d6a1eb293830f70c593ea90fce0cf
lurkydismal <lurkydismal@duck.com>
2026-05-20 07:20:02 +0000

    fix(config/next): rename logging(browser debug info to terminal) to logging(browser to terminal)
    Because NextJS updated it's naming

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
     cacheComponents: true,
 
     logging: {
-        browserDebugInfoInTerminal: true,
+        browserToTerminal: true,
     },
 
     // Disable ONLY if nginx has enabled brotli

86e715094bc5d673d483d35e0f41a739c7f39d07
LurkyDismal <lurkydismal@duck.com>
2026-06-11 14:11:21 +0000

    feat(auth): enable email/password auth and safe-action auth client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,14 +3,25 @@ import { nextCookies } from "better-auth/next-js";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "@/db";
 
-export const auth = betterAuth({
+const auth = betterAuth({
     // ...your config (database, plugins, etc.)
     database: drizzleAdapter(db, {
         provider: "pg", // or "sqlite"
     }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    session: {
+        cookieCache: {
+            enabled: true,
+            strategy: "jwt",
+        }
+    },
     experimental: { joins: true },
     plugins: [
         // ...other plugins
         nextCookies(), // must be the last plugin in the array
     ],
 });
+
+export default auth;
